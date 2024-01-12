a6d5b5a533607b565085d876c35c815e6f510df4
Adhitya Nasuwa <dhitz.nswa@gmail.com>
2024-01-12 08:28:33 +0700

    navbar re-pair

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -5,28 +5,27 @@ import Products from "./products";
 import { Divider } from "primereact/divider";
 
 export default function ProductsList() {
-  const [listActive, setListActive] = useState("product");
+  const [listActive, setListActive] = useState("Products");
   return (
     <div className="mt-10">
       <div className="flex gap-4">
         <Button
           label="Products"
           size="small"
-          className="tracking-wider"
           raised
-          text
-          onClick={(e) => setListActive("product")}
+          onClick={() => setListActive("Products")}
         />
         <Button
           label="Services"
           size="small"
-          className="tracking-wider"
           raised
-          text
-          onClick={(e) => setListActive("service")}
+          severity="danger"
+          onClick={() => setListActive("Services")}
         />
       </div>
-      <Divider />
+      <Divider>
+        <span className="p-tag tracking-wider">{listActive}</span>
+      </Divider>
       <div className="mt-4">
         <Products />
       </div>
--- src/components/ProductsList/products.jsx
+++ src/components/ProductsList/products.jsx
@@ -2,7 +2,79 @@ import React from "react";
 
 export default function Products() {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="w-full border border-slate-300 shadow-md rounded-md">
+        <img
+          src="https://source.unsplash.com/random/300x200"
+          alt=""
+          className="w-full h-[200px] object-cover rounded-t-md"
+        />
+        <div className="p-2">
+          <h2 className="line-clamp-1 font-bold">Apple Mac Book 14 inc</h2>
+          <p className="line-clamp-3 text-xs text-justify text-slate-700 my-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            sapiente excepturi, voluptatibus repellat id harum velit vel
+            possimus at commodi.
+          </p>
+          <hr className="mb-2" />
+          <div className="flex justify-between">
+            <div className="text-xs">
+              <p>1000 terjual</p>
+            </div>
+            <div className="text-xs font-semibold">
+              <i className="fa-solid fa-star text-yellow-500"></i> 4.5
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full border border-slate-300 shadow-md rounded-md">
+        <img
+          src="https://source.unsplash.com/random/300x200"
+          alt=""
+          className="w-full h-[200px] object-cover rounded-t-md"
+        />
+        <div className="p-2">
+          <h2 className="line-clamp-1 font-bold">Apple Mac Book 14 inc</h2>
+          <p className="line-clamp-3 text-xs text-justify text-slate-700 my-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            sapiente excepturi, voluptatibus repellat id harum velit vel
+            possimus at commodi.
+          </p>
+          <hr className="mb-2" />
+          <div className="flex justify-between">
+            <div className="text-xs">
+              <p>1000 terjual</p>
+            </div>
+            <div className="text-xs font-semibold">
+              <i className="fa-solid fa-star text-yellow-500"></i> 4.5
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full border border-slate-300 shadow-md rounded-md">
+        <img
+          src="https://source.unsplash.com/random/300x200"
+          alt=""
+          className="w-full h-[200px] object-cover rounded-t-md"
+        />
+        <div className="p-2">
+          <h2 className="line-clamp-1 font-bold">Apple Mac Book 14 inc</h2>
+          <p className="line-clamp-3 text-xs text-justify text-slate-700 my-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            sapiente excepturi, voluptatibus repellat id harum velit vel
+            possimus at commodi.
+          </p>
+          <hr className="mb-2" />
+          <div className="flex justify-between">
+            <div className="text-xs">
+              <p>1000 terjual</p>
+            </div>
+            <div className="text-xs font-semibold">
+              <i className="fa-solid fa-star text-yellow-500"></i> 4.5
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="w-full border border-slate-300 shadow-md rounded-md">
         <img
           src="https://source.unsplash.com/random/300x200"
